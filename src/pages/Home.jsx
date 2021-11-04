5fa9deeb0bea46de9844968c9f7057ee0953804b
adecodess <ademosuu@gmail.com>
2021-11-04 21:47:32 +0100

    completed annoncement and navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Annoncement from '../components/Annoncement';
+import Navbar from '../components/Navbar';
+import Slider from '../components/Slider';
+
+const Home = () => {
+  return (
+    <div>
+      <Annoncement />
+      <Navbar />
+      <Slider />
+    </div>
+  );
+};
+
+export default Home;
