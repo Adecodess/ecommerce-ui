a2eb5b06bd48b7c317124d2e31d79908117a0ea5
adecodess <ademosuu@gmail.com>
2021-11-05 21:04:22 +0100

    completed slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,12 +1,15 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
-
 import { ArrowRightOutlined, ArrowLeftOutlined } from '@mui/icons-material';
+import Img from '../assets/images/bg.png';
+import { sliderItems } from '../data';
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -25,24 +28,89 @@ const Arrow = styled.div`
   margin: auto;
   cursor: pointer;
   opacity: 0.5;
+  z-index: 2;
 `;
 
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
+  transition: all 2s ease;
+  transform: translateX(${(props) => props.slide * -100}vw);
 `;
 
-const Slide = styled.div``;
-const ImgContainer = styled.div``;
-const InfoContainer = styled.div``;
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${(props) => props.bg};
+`;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
 
 const Slider = () => {
+  const [slide, setSlide] = useState(0);
+
+  const handleClick = (direction) => {
+    if (direction === 'left') {
+      setSlide(slide > 0 ? slide - 1 : 2);
+    } else {
+      setSlide(slide < 2 ? slide + 1 : 0);
+    }
+  };
+
   return (
     <Container>
-      <Arrow direction='left'>
+      <Arrow direction='left' onClick={() => handleClick('left')}>
         <ArrowLeftOutlined />
       </Arrow>
-      <Wrapper></Wrapper>
-      <Arrow direction='right'>
+      <Wrapper slide={slide}>
+        {sliderItems.map((item) => {
+          return (
+            <>
+              <Slide bg={item.bg} key={item.id}>
+                <ImgContainer>
+                  <Image src={item.img} />
+                </ImgContainer>
+                <InfoContainer>
+                  <Title>{item.title}</Title>
+                  <Desc>{item.desc}</Desc>
+                  <Button>SHOP NOW</Button>
+                </InfoContainer>
+              </Slide>
+              ;
+            </>
+          );
+        })}
+      </Wrapper>
+      <Arrow direction='right' onClick={() => handleClick('right')}>
         <ArrowRightOutlined />
       </Arrow>
     </Container>
---
+++ src/data.js
@@ -0,0 +1,27 @@
+import Img1 from './assets/images/bg.png';
+import Img2 from './assets/images/2.jpg';
+import Img3 from './assets/images/3.jpg';
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: Img1,
+    title: 'SUMMER SALE',
+    desc: ' DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS',
+    bg: 'f5fafd',
+  },
+  {
+    id: 2,
+    img: Img2,
+    title: 'AUTUMN COLLECTION',
+    desc: ' DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS',
+    bg: 'fcf1ed',
+  },
+  {
+    id: 3,
+    img: Img3,
+    title: 'WINTER SALE',
+    desc: ' DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS',
+    bg: 'fbf0f4',
+  },
+];
