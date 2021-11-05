2b2c657ff9e2107864c1833caf4349722bbd4c80
adecodess <ademosuu@gmail.com>
2021-11-05 21:41:48 +0100

    completed categories

--- src/data.js
+++ src/data.js
@@ -2,6 +2,10 @@ import Img1 from './assets/images/bg.png';
 import Img2 from './assets/images/2.jpg';
 import Img3 from './assets/images/3.jpg';
 
+import Img4 from './assets/images/shirt.jpg';
+import Img5 from './assets/images/gown.jpg';
+import Img6 from './assets/images/jacket.jpg';
+
 export const sliderItems = [
   {
     id: 1,
@@ -23,5 +27,23 @@ export const sliderItems = [
     title: 'WINTER SALE',
     desc: ' DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS',
     bg: 'fbf0f4',
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: Img4,
+    title: 'SHIRT STYLE',
+  },
+  {
+    id: 2,
+    img: Img5,
+    title: 'DINNER GOWN',
+  },
+  {
+    id: 3,
+    img: Img6,
+    title: 'WINTER JACKETS',
   },
 ];
